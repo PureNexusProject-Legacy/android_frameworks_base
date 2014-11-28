2fd94196fb6fc79c1f0ac495670157fae551b47e
Aaron Echols <atechols@gmail.com>
2014-11-28 18:39:26 -0500

    framework: port IME selector notification toggle (2/2)
    Change-Id: Ia35960d88468dba44cdfad26c7cc74f811a6844f

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2119,6 +2119,12 @@ public final class Settings {
         public static final String RECENTS_LONGPRESS = "recents_longpress";
 
         /**
+         * Whether to show the IME switcher in the status bar
+         * @hide
+         */
+        public static final String STATUS_BAR_IME_SWITCHER = "status_bar_ime_switcher";
+
+        /**
          * Whether to allow one finger quick settings expansion on the right side of the statusbar.
          *
          * @hide
