4d985ca5b434366f03df9562a01c4a51bfa53b17
Roman Birg <roman@cyngn.com>
2015-05-28 14:09:42 +0000

    Revert "Better density scaling compatibility"
    This isn't the proper solution and seems to cause even more scaling issues with old apps.
    
    This reverts commit f61367b4bf2462ada18dfeac1c4325f749bac47b.
    
    Change-Id: Iafee3dd0409496622557ced3c8b3cf80c5c0faad

--- core/java/android/content/res/Resources.java
+++ core/java/android/content/res/Resources.java
@@ -250,7 +250,7 @@ public class Resources {
             CompatibilityInfo compatInfo, IBinder token) {
         mAssets = assets;
         mMetrics.setToDefaults();
-        mMetrics.forcePersistedDensity();
+        mMetrics.updateDensity();
         if (compatInfo != null) {
             mCompatibilityInfo = compatInfo;
         }
@@ -1804,14 +1804,8 @@ public class Resources {
             if (mConfiguration.densityDpi != Configuration.DENSITY_DPI_UNDEFINED) {
                 mMetrics.densityDpi = mConfiguration.densityDpi;
                 mMetrics.density = mConfiguration.densityDpi * DisplayMetrics.DENSITY_DEFAULT_SCALE;
-
-                final boolean densityScalingSupported = !mCompatibilityInfo.isScalingRequired()
-                        || mCompatibilityInfo.alwaysSupportsScreen();
-                final boolean appScalingRequired = mCompatibilityInfo.neverSupportsScreen()
-                        && mCompatibilityInfo.isScalingRequired();
-                if (densityScalingSupported || appScalingRequired ||
-                        mCompatibilityInfo.equals(CompatibilityInfo.DEFAULT_COMPATIBILITY_INFO)) {
-                    mMetrics.forcePersistedDensity();
+                if (mCompatibilityInfo.equals(CompatibilityInfo.DEFAULT_COMPATIBILITY_INFO)) {
+                    mMetrics.updateDensity();
                 }
             }
             mMetrics.scaledDensity = mMetrics.density * mConfiguration.fontScale;
