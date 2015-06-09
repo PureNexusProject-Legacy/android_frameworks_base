60c81475cd2df2f4961e4cd2c22dc0d9c2ad65c0
beanstown106 <nbenis106@gmail.com>
2015-06-09 00:06:11 -0400

    Revert "frameworks/base: Fix binder parcel leak with getMetadata() API"
    This reverts commit 55b813386b3ca54e58c34b663f1faafe7e6c2d64.

--- media/java/android/media/Metadata.java
+++ media/java/android/media/Metadata.java
@@ -542,12 +542,4 @@ import java.util.TimeZone;
             throw new IllegalStateException("Wrong type " + expectedType + " but got " + type);
         }
     }
-
-    /**
-     * Recycle parcel object
-     */
-    public void recycleParcel() {
-        if (mParcel != null)
-            mParcel.recycle();
-    }
 }
