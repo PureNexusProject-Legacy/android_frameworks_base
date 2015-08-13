3119d2500ded19686dc9a20680c8c21bd443148f
beanstown106 <nbenis106@gmail.com>
2015-08-13 17:45:01 -0400

    Revert "platform/framework/base: Fix Half Blank screen issue in Wallpaper"
    This reverts commit 27a65eb5aba0d6ac9a57bc96983e2f463e3a5ad9.

--- packages/SystemUI/src/com/android/systemui/ImageWallpaper.java
+++ packages/SystemUI/src/com/android/systemui/ImageWallpaper.java
@@ -116,7 +116,6 @@ public class ImageWallpaper extends WallpaperService {
         boolean mVisible = true;
         boolean mRedrawNeeded;
         boolean mOffsetsChanged;
-        boolean mSurfaceChanged;
         int mLastXTranslation;
         int mLastYTranslation;
 
@@ -276,7 +275,7 @@ public class ImageWallpaper extends WallpaperService {
             }
 
             super.onSurfaceChanged(holder, format, width, height);
-            mSurfaceChanged = true;
+
             drawFrame();
         }
 
@@ -319,10 +318,9 @@ public class ImageWallpaper extends WallpaperService {
                 // Sometimes a wallpaper is not large enough to cover the screen in one dimension.
                 // Call updateSurfaceSize -- it will only actually do the update if the dimensions
                 // should change
-                if (newRotation != mLastRotation || mSurfaceChanged ) {
+                if (newRotation != mLastRotation) {
                     // Update surface size (if necessary)
                     updateSurfaceSize(getSurfaceHolder());
-                mSurfaceChanged = false;
                 }
                 SurfaceHolder sh = getSurfaceHolder();
                 final Rect frame = sh.getSurfaceFrame();
