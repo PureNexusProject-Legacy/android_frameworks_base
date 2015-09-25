5a8e17ceadbfb4431bdbcebec4b8aab2119a1d57
LorDClockaN <davor@losinj.com>
2015-09-25 14:33:05 +0200

    Fix COS12 themer app FCing
    Use parts of this patch:
    https://github.com/CyanogenMod/android_frameworks_base/commit/f6ede14aaedb430eaf26e89ccbe33c5372a0dff1
    
    I reverted that patch to get hw keys switch working
    
    Change-Id: I82a193d68ab248b745684d4fd953d29fd921637d

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1809,6 +1809,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 && !mAccessibilityManager.isTouchExplorationEnabled();
     }
 
+    public boolean needsNavigationBar() {
+        return mHasNavigationBar;
+    }
+
     @Override
     public boolean isDefaultOrientationForced() {
         return mForceDefaultOrientation;
