4ab2fc08bffe6fc4a766bd6791006c9eb769aa62
Danesh M <daneshm90@gmail.com>
2015-10-19 15:06:20 -0400

    Framework: Forward port Long press back to kill app (2/2)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6739,6 +6739,12 @@ public final class Settings {
         public static final String QS_USE_MAIN_TILES = "sysui_qs_main_tiles";
 
         /**
+         * Whether to allow killing of the foreground app by long-pressing the Back button
+         * @hide
+         */
+        public static final String KILL_APP_LONGPRESS_BACK = "kill_app_longpress_back";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
