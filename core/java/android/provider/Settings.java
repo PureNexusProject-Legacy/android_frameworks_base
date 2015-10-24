b71dfa3b7e8ef4601deb4f7d42850494fa609aeb
Konsta <konsta09@gmail.com>
2015-10-24 01:49:00 -0400

    Framework: Volume key cursor control (1/2) This feature is moved to framework so it also works with third party keyboards.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3450,6 +3450,15 @@ public final class Settings {
         public static final String VOLBTN_MUSIC_CONTROLS = "volbtn_music_controls";
 
         /**
+         * Volume keys control cursor in text fields (default is 0)
+         * 0 - Disabled
+         * 1 - Volume up/down moves cursor left/right
+         * 2 - Volume up/down moves cursor right/left
+         * @hide
+         */
+        public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
+
+        /**
          * Boolean value whether to link ringtone and notification volume
          * @hide
          */
