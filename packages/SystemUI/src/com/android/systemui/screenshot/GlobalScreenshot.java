720ebc79d4941cee84615ae825711d52f10fc431
Alex Cruz <mazdarider23@gmail.com>
2015-10-28 06:34:19 +0000

    Added a toast to screenshot delete, why not right?
    http://i.imgur.com/vhVqXlv.jpg

--- packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
@@ -56,6 +56,7 @@ import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.view.animation.Interpolator;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.android.systemui.R;
 
@@ -818,6 +819,7 @@ class GlobalScreenshot {
             final Uri uri = Uri.parse(intent.getStringExtra(SCREENSHOT_URI_ID));
             nm.cancel(id);
 
+            Toast.makeText(context, R.string.delete_screenshot_toast, Toast.LENGTH_SHORT).show();
             // And delete the image from the media store
             new DeleteImageInBackgroundTask(context).execute(uri);
         }
