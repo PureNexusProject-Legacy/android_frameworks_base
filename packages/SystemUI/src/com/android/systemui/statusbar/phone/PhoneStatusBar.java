e3bd56774b555ec9e341dacde39baccc31cb556b
Danesh M <daneshm90@gmail.com>
2015-11-09 11:23:00 -0800

    SystemUI : Fix now on tap home button long press
    Change-Id: Ibd4d8e62d82c43c53bb61fc5ee92053d0f50bc38

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1213,7 +1213,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mNavigationBarView.reorient();
 
         mNavigationBarView.setListeners(mRecentsClickListener, mRecentsPreloadOnTouchListener,
-                mLongPressBackRecentsListener, mHomeActionListener);
+                mLongPressBackRecentsListener, mHomeActionListener, mLongPressHomeListener);
         mAssistManager.onConfigurationChanged();
     }
 
