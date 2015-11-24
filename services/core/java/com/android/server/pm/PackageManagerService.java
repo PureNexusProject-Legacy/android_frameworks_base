60892d0a3bb2f474ca46481c3170c4215fe65136
dhacker29 <dhackerdvm@gmail.com>
2015-11-24 01:53:47 -0500

    Core: Use ro.purenexus.version to signal mIsUpgrade
    We use a static fingerprint that is only changed when a new stock
    version is released, so every flash shows Android is starting instead
    of upgrading.  This will fix that.

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -2053,7 +2053,7 @@ public class PackageManagerService extends IPackageManager.Stub {
             }
 
             final VersionInfo ver = mSettings.getInternalVersion();
-            mIsUpgrade = !Build.FINGERPRINT.equals(ver.fingerprint);
+            mIsUpgrade = !Build.NEXUS.equals(ver.fingerprint);
             // when upgrading from pre-M, promote system app permissions from install to runtime
             mPromoteSystemApps =
                     mIsUpgrade && ver.sdkVersion <= Build.VERSION_CODES.LOLLIPOP_MR1;
@@ -2320,7 +2320,7 @@ public class PackageManagerService extends IPackageManager.Stub {
                         deleteCodeCacheDirsLI(ps.volumeUuid, ps.name);
                     }
                 }
-                ver.fingerprint = Build.FINGERPRINT;
+                ver.fingerprint = Build.NEXUS;
             }
 
             checkDefaultBrowser();
@@ -15817,7 +15817,7 @@ public class PackageManagerService extends IPackageManager.Stub {
                     Slog.w(TAG, "Failed to scan " + ps.codePath + ": " + e.getMessage());
                 }
 
-                if (!Build.FINGERPRINT.equals(ver.fingerprint)) {
+                if (!Build.NEXUS.equals(ver.fingerprint)) {
                     deleteCodeCacheDirsLI(ps.volumeUuid, ps.name);
                 }
             }
