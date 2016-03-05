4cba2eb29de3d7e0601dd3047d10c547e17ceeab
beanstown106 <nbenis106@gmail.com>
2016-03-05 00:08:49 -0500

    PureAmbient display [1/2] *This is a rehash of parts of slims and parts of omni's ambient display mods *Added my own little twist on some of the things
    Features:
    *default/user config switch
    *Fade in duration
    *Fade out duration
    *Visibility duration
    *Option to disable pulse on notifications
    *Option to disable pulse with pickup sensor
    
    Credits:
    Maxwen(omni)
    Squadzone(omni)
    Dankoman(slim)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3231,6 +3231,48 @@ public final class Settings {
         public static final String ACCELEROMETER_ROTATION_ANGLES = "accelerometer_rotation_angles";
 
         /**
+         * Enable/disable overwrite default value for ambient display
+         * @hide
+         */
+        public static final String DOZE_OVERWRITE_VALUE = "doze_overwrite_value";
+
+        /**
+         * Duration in for ambient display
+         * @hide
+         */
+        public static final String DOZE_PULSE_DURATION_IN = "doze_pulse_duration_in";
+
+        /**
+         * Duration out for ambient display
+         * @hide
+         */
+        public static final String DOZE_PULSE_DURATION_OUT = "doze_pulse_duration_out";
+
+        /**
+         * Duration visible for ambient display
+         * @hide
+         */
+        public static final String DOZE_PULSE_DURATION_VISIBLE = "doze_pulse_duration_visible";
+
+        /**
+         * Pulse if there is notifications for ambient display
+         * @hide
+         */
+        public static final String DOZE_PULSE_ON_NOTIFICATIONS = "doze_pulse_on_notifications";
+
+        /**
+         * Pulse if the pickup sensor detects movement
+         * @hide
+         */
+        public static final String DOZE_PULSE_ON_PICKUP = "doze_pulse_on_pickup";
+
+        /**
+         * custom screen brightness for doze mode
+         * @hide
+         */
+        public static final String DOZE_SCREEN_BRIGHTNESS = "doze_screen_brightness";
+
+        /**
          * Control whether the accelerometer will be used to change lockscreen
          * orientation.  If 0, it will not be used; if 1, it will be used by default.
          * @hide
