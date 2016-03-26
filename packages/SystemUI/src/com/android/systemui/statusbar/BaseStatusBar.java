86a59b99bc87699991c8311a1f79b7f6ce83a774
BigBrother1984 <carlosavignano@aospa.co>
2016-03-26 16:55:22 -0400

    Improve Heads up toggle [1/2] * Add multiuser support, provide a system setting to override heads up   showing. User can disable heads up within this one so we don't need to   mess with Settings.Global heads up dependencies and break Google wide   implementations like priority filtering etc. *updated heads up quicksetting tile to reflect changes as well
    was originally done by @BigBrother1984 <carlosavignano@aospa.co> in LP
    just pulled out the toggle part from this commit AOSPA/android_frameworks_base@b62eeed

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -182,6 +182,7 @@ public abstract class BaseStatusBar extends SystemUI implements
     private float mFontScale;
 
     protected boolean mUseHeadsUp = false;
+    protected boolean mHeadsUpUserEnabled = false;
     protected boolean mHeadsUpTicker = false;
     protected boolean mDisableNotificationAlerts = false;
 
@@ -2016,7 +2017,7 @@ public abstract class BaseStatusBar extends SystemUI implements
             entry.icon.set(ic);
             inflateViews(entry, mStackScroller);
         }
-        if (mUseHeadsUp) {
+        if (mUseHeadsUp && mHeadsUpUserEnabled) {
             updateHeadsUp(key, entry, shouldInterrupt, alertAgain);
         }
         mNotificationData.updateRanking(ranking);
