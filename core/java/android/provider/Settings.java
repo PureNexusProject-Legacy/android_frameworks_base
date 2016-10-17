22e01e75e2679a680ad64bd4b84881ba84ceda2b
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-10-17 22:00:05 +0800

    SystemUI: Implement battery level text customisations
    3 options - hidden, show beside, or show inside.
    
    Forward port the battery text-related changes from CM 13.0:
    2e0a11bb0bbf22c32f6e15d97ccac10414ec7b44 Forward port battery icon options
    738f5a87a361c9c1027e3764072a37a50467fe6d SystemUI: Expose values in status bar expanded header
    9500061a111fed25bed3ad30173d8e35063f32d0 Themes: Expose hard code for themes in SystemUI
    
    Change-Id: Idec48a3536a3dd529f7dd6076bf33a20cd8020c1

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6905,6 +6905,15 @@ public final class Settings {
          */
         public static final String KILL_APP_LONGPRESS_BACK = "kill_app_longpress_back";
 
+       /**
+         * Status bar battery %
+         * 0: Hide the battery percentage
+         * 1: Display the battery percentage inside the icon
+         * 2: Display the battery percentage next to the icon
+         * @hide
+         */
+        public static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
+
         /**
          * This are the settings to be backed up.
          *
@@ -6988,7 +6997,8 @@ public final class Settings {
             QS_TILES,
             DOZE_ENABLED,
             DOZE_PULSE_ON_PICK_UP,
-            DOZE_PULSE_ON_DOUBLE_TAP
+            DOZE_PULSE_ON_DOUBLE_TAP,
+            STATUS_BAR_SHOW_BATTERY_PERCENT
         };
 
         /**
