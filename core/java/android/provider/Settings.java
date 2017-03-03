rcstar6696/android_frameworks_base_pure

incall vibration options [2/3]

core/java/android/provider/Settings.java
@@ -4209,6 +4209,18 @@ public final class Settings {
         public static final String TOAST_ICON = "toast_icon";
 
         /**
+         * Whether the phone vibrates on call awnsered
+         * @hide
+         */
+        public static final String VIBRATE_ON_AWNSER = "vibrate_on_awnser";
+
+        /**
+         * Whether the phone vibrates on hangup
+         * @hide
+         */
+        public static final String VIBRATE_ON_HANGUP = "vibrate_on_hangup";
+
+        /**
          * Whether the phone volume up / down effect should be played
          * @hide
          */

PureNexusProject-Legacy/android_frameworks_base

core/java/android/provider/Settings.java
@@ -4209,6 +4209,24 @@ public final class Settings {
         public static final String TOAST_ICON = "toast_icon";
 
         /**
+         * Whether the phone vibrates on call connect
+         * @hide
+         */
+        public static final String VIBRATE_ON_CONNECT = "vibrate_on_connect";
+
+        /**
+         * Whether the phone vibrates on call waiting
+         * @hide
+         */
+        public static final String VIBRATE_ON_CALLWAITING = "vibrate_on_callwaiting";
+
+        /**
+         * Whether the phone vibrates on disconnect
+         * @hide
+         */
+        public static final String VIBRATE_ON_DISCONNECT = "vibrate_on_disconnect";
+
+        /**
          * Whether the phone volume up / down effect should be played
          * @hide
          */